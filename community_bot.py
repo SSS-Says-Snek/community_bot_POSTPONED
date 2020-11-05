09738945b780f803173a2886450f197936acbe26
Brandon <bmcomis2018@gmail.com>
2020-11-05 08:18:06 -0600

    failed....

--- community_bot.py
+++ community_bot.py
@@ -54,7 +54,7 @@ async def changestatus(ctx, status):
 # for filename in os.listdir('./cogs'): if filename.endswith('.py'): bot.load_extension(f"cogs.{filename[:-3]}")
 # TODO: I think that I can
 #  now deploy it to heroku. I just have to remember to specify the path file things more. I hope. Do this in the MORNINGGGGGGG
-extensions = [r'C:\Users\Admin\AppData\Local\Programs\Python\Python38\Discord Code\Community Bot\cogs.BuiltInCogs']
+extensions = ['cogs.BuiltInCogs']
 
 if __name__ == '__main__':
     for ext in extensions:
